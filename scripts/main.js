d1280af6cf196ce2878563626e79440ab4cf19cc
Kibet <quibetamos@gmail.com>
2019-02-16 12:42:55 +0300

    Use CSS to change the background color of the body

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+function measurements() {
+var a=parseInt(document.getElementById("a")).value;
+var b=parseInt(document.getElementById("b")).value;
+var c =parseInt(document.getElementById("c")).value;
+if ( a+b <= c || b+c <= a || a+c <= b){
+  alert("Cannot form a triangle.");
+}
+else if(a===b&&b==c){
+  alert("equilateral");
+}
+else if (a==b || a==c || b==c) {
+  alert("isoceles");
+}
+else {
+  alert("scalene");
+}
+}
